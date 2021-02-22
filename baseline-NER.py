6d25ee17bca9687fdb57046e7ce5b86812089599
Marcel Pons <mponsclo7@gmail.com>
2021-02-22 20:24:54 +0100

    pass code to .py program

--- baseline-NER.py
+++ baseline-NER.py
@@ -6,7 +6,13 @@ import string
 from os import listdir
 from xml.dom.minidom import parse
 from nltk.tokenize import word_tokenize
-from evaluator import evaluate
+from nltk.corpus import stopwords
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+#from evaluator import evaluate
 
 ## ------------- Tokenize Sentence -------------
 def tokenize(s):
@@ -16,13 +22,48 @@ def tokenize(s):
     Input - s: string containing the text for one sentence
     Output - Returns a list of tuples (word , offsetFrom , offsetTo )'''
 
-    offset = 0
+    token_list = []
+    tokens = word_tokenize(s)
+    stop_words = set(stopwords.words("english"))
     
+    for t in tokens:
+        if (t in stop_words) | (not t.isalpha()): # reduce workload
+            continue
+        else:
+            offsetFrom = s.find(t)
+            offsetTo = offsetFrom + len(t) - 1
+            token_list.append((t, offsetFrom, offsetTo))
+            
+    return token_list
 
+sent = tokenize("Phenothiazines and butyrophenones may reduce or reverse the depressor effect of epinephrine.")
+print(sent) 
 
 ## ------------- Classify Token -------------
+def token_type_classifier(word):
+        
+    threes = ["nol", "lol", "hol", "lam", "pam"]
+    fours = ["arin", "oxin", "toin","pine", "tine", "bital", "inol", "pram"]
+    fives = ["azole", "idine", "orine", "mycin", "hrine", "exate", "amine", "emide"]
 
+    groups = ["depressants", "steroid", "ceptives", "urates", "amines", "azines", "phenones", 
+              "inhib", "coagul", "block", "acids", "agent"]
+    
+    if word.isupper() & (len(word) >= 4): 
+        return True, "brand"  # add word[0].isupper ?
+    elif (word[-3:] in threes) | (word[-4:] in fours) | (word[-5:] in fives):
+        return True, "drug"
+    elif (True in [t in word for t in groups]) | ((word[-1:] == "s") & (word[-2].isupper())) | (word.isupper() & (len(word) < 4)): 
+        return True, "group"
+    #elif word in drug_set:        # Drug Database Checking
+    #    return True, "drug"
+    else: 
+        return False, ""
 
+print(token_type_classifier("NSAIDs"))
+print(sent)
+for t in sent[]:
+    print(token_type_classifier(t))
 
 ## ------------- Entity Extractor -------------
 def extract_entities(s):
@@ -30,28 +71,48 @@ def extract_entities(s):
     Input - s: A tokenized sentence ( list of triples (word , offsetFrom , offsetTo ) )
     Output - A list of entities . Each entity is a dictionary with the keys 'name ', ' offset ', and 'type '''
 
+    output = []
+    for t in s:
+        tokenText = t[0] # get the only the text from (text, offsetFrom, offsetTo)
+        (is_brand_drug_group, type_text) = token_type_classifier(tokenText)
+        
+        if is_brand_drug_group:
+            offsetFrom = t[1]
+            offsetTo = t[2]
+            entity = {"name" : tokenText,
+                     "offset" : str(offsetFrom) + "-" + str(offsetTo), 
+                     "type" : type_text}
+            output.append(entity)
+    
+    return(output)
 
-
-def main(datadir, outfile):
+def main(datadir): #, outfile):
     '''datadir - directory with XML files
        outfile - name for the outputfile'''
 
     # process each file in directory
     for f in listdir(datadir):
-        # parse XML file, obtaining a DOM tree
-        tree = parse(datadir + "/" + f)
-        # process each senetence in the file
-        sentences = tree.getElementsByTagName("sentence")
-        for s in sentences:
-                sid = s.attributes["id"].value        # get sentence id
-                stext = s.attributes["text"].value    # get sentence text
-                # tokenize text
-                tokens = tokenize(stext)
-                # extract entities from tokenized sentence text
-                entities = extract_entities(tokens)
+        try:
+            # parse XML file, obtaining a DOM tree
+            tree = parse(datadir + "/" + f)
+            # process each senetence in the file
+            sentences = tree.getElementsByTagName("sentence")
+            for s in sentences:
+                    sid = s.attributes["id"].value        # get sentence id
+                    stext = s.attributes["text"].value    # get sentence text
+                    # tokenize text
+                    tokens = tokenize(stext)
+                    # extract entities from tokenized sentence text
+                    entities = extract_entities(tokens)
 
-                # print sentence entities in format requested for evaluation
-                for e in entities:
-                    print(sid + "|" + e["offset"] + "|" + e["text"] + "|" e["type"], file = outf)
+                    # print sentence entities in format requested for evaluation
+                    for e in entities:
+                        print(sid + "|" + e["offset"] + "|" + e["text"] + "|" + e["type"]) #, file = outf)
+        except:
+            pass
         # print performance score
-        evaluator.evaluate("NER", datadir, outfile)
+        #evaluator.evaluate("NER", datadir, outfile)
+
+datadir = "/Users/mponsclo/Documents/DataScience/ALHT_Project/train"
+result = main(datadir)
+print(result)
