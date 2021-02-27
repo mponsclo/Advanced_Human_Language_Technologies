1fa9d27b30ef127d3c68e7074ccf0fda282cdb0f
zoftar <zosiarrf@gmail.com>
2021-02-27 22:00:35 +0100

    cleanup code

--- baseline-NER.py
+++ baseline-NER.py
@@ -1,28 +1,22 @@
 '''Program that parses all XML files in the folder given as argument and recognizes and classifies drug names. 
 Based on simple heuristic rules'''
 
-import sys
 import string
 import evaluator
+import argparse
 from os import listdir
 from xml.dom.minidom import parse
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
 
 
-
-
-## ------------- Tokenize Sentence -------------
 def tokenize(s):
     '''
     Given a sentence , calls nltk.tokenize to split it in tokens, and adds to each token its start / end offset 
     in the original sentence .
     Input - s: string containing the text for one sentence
-    Output - Returns a list of tuples (word , offsetFrom , offsetTo )'''
+    Output - Returns a list of tuples (word , offsetFrom , offsetTo )
+    '''
 
     token_list = []
     tokens = word_tokenize(s)
@@ -38,34 +32,40 @@ def tokenize(s):
 
     return token_list
 
-
-# sent = tokenize("Phenothiazines and butyrophenones may reduce or reverse the depressor effect of epinephrine.")
-# print(sent)
-
 def read_drug_list_file(resource_path):
-    
-    drug_set = set()
-    with open(resource_path, 'r') as resource_file:
-        drugs = resource_file.readlines()
-        drug_set = set([d[:-1].lower() for d in drugs])
-    return drug_set
+    '''
+    Read the HSDB drug database.
+    Input:
+    resource_path - path to HSDB.txt file
+    Output:
+    drugs - set of drugs
+    '''
+    resource_file = open(resource_path, 'r')
+    lines = resource_file.readlines()
+    drugs = set([d[:-1].lower() for d in lines])
+    return drugs
 
-## ------------- Classify Token -------------
-def token_type_classifier(word):
+def token_type_classifier(word, drug_set, should_look_up=False):
+    '''
+    Classify the tokens.
+    Input:
+    word - string: token to classify
+    drug_set - list of drugs to look up the drugs
+    should_look_up - True when words should be looked up in the drug database
+    Output:
+    tuple (b, s) - b is a boolean value, True when the word is classiied to one of the groups. s is a string - name of the group
+    '''
     threes = ["nol", "lol", "hol", "lam", "pam"]
     fours = ["arin", "oxin", "toin", "pine", "tine", "bital", "inol", "pram"]
     fives = ["azole", "idine", "orine", "mycin", "hrine", "exate", "amine", "emide"]
 
     groups = ["depressants", "steroid", "ceptives", "urates", "amines", "azines", "phenones",
               "inhib", "coagul", "block", "acids", "agent"]
-    resource_path ="../labAHLT/resources/HSDB.txt"
-
-    #drug_set = read_drug_list_file(resource_path)
 
     if word.isupper() & (len(word) >= 4):
-        return True, "brand"  # add word[0].isupper ? --- i don't think so, every sentence starts with an uppercase letter
-    #elif word in drug_set:        # Drug Database Checking
-    #    return True, "drug"
+        return True, "brand"
+    elif should_look_up & (word in drug_set):   
+        return True, "drug"
     elif (word[-3:] in threes) | (word[-4:] in fours) | (word[-5:] in fives):
         return True, "drug"
     elif (True in [t in word for t in groups]) | ((word[-1:] == "s") & (word[-2].isupper())) | (
@@ -76,44 +76,44 @@ def token_type_classifier(word):
         return False, ""
 
 
-# print(token_type_classifier("NSAIDs"))
-# print(sent[0])
-# for t in sent:
-#    tokenText = t[0]
-#    print(token_type_classifier(tokenText))
-
-## ------------- Entity Extractor -------------
-def extract_entities(s):
-    ''' Given a tokenized sentence , identify which tokens (or groups of consecutive tokens ) are drugs
-    Input - s: A tokenized sentence ( list of triples (word , offsetFrom , offsetTo ) )
-    Output - A list of entities . Each entity is a dictionary with the keys 'name ', ' offset ', and 'type '''
+def extract_entities(sentence, drug_set, should_look_up):
+    '''
+    Given a tokenized sentence , identify which tokens (or groups of consecutive tokens ) are drugs
+    Input:
+    sentence - A tokenized sentence (list of triples (word, offsetFrom, offsetTo))
+    drug_set - list of drugs to look up the drugs
+    should_look_up - True when words should be looked up in the drug database
+    Output:
+    output - A list of entities. Each entity is a dictionary with the keys 'name', 'offset', and 'type'
+    '''
 
     output = []
-    for t in s:
-        tokenText = t[0]  # get the only the text from (text, offsetFrom, offsetTo)
-        (is_brand_drug_group, type_text) = token_type_classifier(tokenText)
+    for t in sentence:
+        token_text = t[0]  # get the only the text from (text, offset_from, offset_to)
+        (is_brand_drug_group, type_text) = token_type_classifier(token_text, drug_set, should_look_up)
 
         if is_brand_drug_group:
-            offsetFrom = t[1]
-            offsetTo = t[2]
-            entity = {"name": tokenText,
-                      "offset": str(offsetFrom) + "-" + str(offsetTo),
+            offset_from = t[1]
+            offset_to = t[2]
+            entity = {"name": token_text,
+                      "offset": str(offset_from) + "-" + str(offset_to),
                       "type": type_text}
             output.append(entity)
 
     return (output)
 
+def main(datadir, outfile, drug_path, should_look_up=False):
+    '''
+    Main function.
+    Input:
+    datadir - directory with XML files
+    outfile - name for the output file
+    drug_path - path to the drug database file
+    should_look_up - bollean indicating if the words should be looked up in a drug database
+    '''
 
-# entity = extract_entities(sent)
-# print(entity)
-
-# for f in listdir(datadir):
-#    print(f)
-
-def main(datadir, outfile):
-    '''datadir - directory with XML files
-       outfile - name for the outputfile'''
     outf = open(outfile, 'w')
+    drugs = read_drug_list_file(drug_path)
     # process each file in directory
     for f in listdir(datadir):
         try:
@@ -127,12 +127,11 @@ def main(datadir, outfile):
                 # tokenize text
                 tokens = tokenize(stext)
                 # extract entities from tokenized sentence text
-                entities = extract_entities(tokens)
+                entities = extract_entities(tokens, drugs, should_look_up)
 
                 # print sentence entities in format requested for evaluation
                 for e in entities:
                     entity = sid + "|" + e["offset"] + "|" + e["name"] + "|" + e["type"]
-                    # print(entity)
                     outf.write(entity + '\n')
 
         except:
@@ -141,8 +140,10 @@ def main(datadir, outfile):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        raise RuntimeException('Not enough arguments; pass the path to data and the name of the output file')
-    path = sys.argv[1]
-    output_file = sys.argv[2]
-    main(path, output_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_data_path", type=str, help="Path to train data")
+    parser.add_argument("output_file_name", type=str, help="Output file name")
+    parser.add_argument("drug_list_path", type=str, help="Path to list of drugs")
+    parser.add_argument("-l", nargs='?', const=True, default=False, help="Include looking up in the dictionary")
+    args = parser.parse_args()
+    main(args.train_data_path, args.output_file_name, args.drug_list_path, args.l)
