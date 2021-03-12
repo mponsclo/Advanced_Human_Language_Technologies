9003fbda30ef07ffdb39156039f1e3b86c7451c1
zoftar <zosiarrf@gmail.com>
2021-03-13 00:01:51 +0100

    Refactor the learner

--- crf-learner.py
+++ crf-learner.py
@@ -2,64 +2,33 @@
 import argparse
 import sklearn
 import pycrfsuite
-from itertools import groupby
+
 from operator import itemgetter
 
-def sentence2features(sentence):
-    classes = []
-    features = []
-    tokens = []
-    for token in sentence:
-        split_line = token[:-1].split('\t')
-        sentence_id = split_line[0]
-        feature_dict = {
-            "form": split_line[5],
-            "formlower": split_line[6]
-
-        }
-        #TODO read all features from text files
-        features.append(split_line[5:])
-        classes.append(split_line[4])
-        tokens.append((split_line[1], split_line[2], split_line[3]))
-    return features, classes, tokens
-
-def read_feature_file(filepath):
-    f = open(filepath, 'r')
-    lines = f.readlines()
-    features = []
-    classes = []
-    metadata=[] # sentence ids, token form, offsets - for later reconstruction
-    sentences = set()
-    #print(lines)
-    sentences = groupby(lines, lambda l: l.split('\t')[0])
-    for sid, sentence in sentences:
-        s_features, s_classes, s_tokens = sentence2features(sentence)
-        features.append(s_features)
-        classes.append(s_classes)
-        metadata.append((sid, s_tokens))
-           
-    return metadata, features, classes
-
+from utils import read_feature_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("model_name", type=str, help="Name of the model file")
     parser.add_argument("train_file_path", type=str, help = "Path to data")
     args = parser.parse_args()
+
+    # read features from file
     train_metadata, X_train, y_train = read_feature_file(args.train_file_path)
 
-    train_result_file = 'conll2002-esp.crfsuite'
-    trainer = pycrfsuite.Trainer(verbose = False)
-
+    # create trainer and configure it
+    trainer = pycrfsuite.Trainer(verbose=False)
     for xseq, yseq in zip(X_train, y_train):
         trainer.append(xseq, yseq)
 
     trainer.set_params({
         'c1': 1.0,  # coefficient for L1 penalty
         'c2': 1e-3,  # coefficient for L2 penalty
-        'max_iterations': 50,   # stop earlier
+        'max_iterations': 100,
 
         # include transitions that are possible, but not observed
         'feature.possible_transitions': True
     })
 
-    trainer.train(train_result_file)
+    #train the model and save it
+    trainer.train(args.model_name)
---
+++ utils.py
@@ -0,0 +1,55 @@
+from itertools import groupby
+
+# helper functions
+
+
+def parse_sentence_string(sentence):
+    '''
+    Task:
+        Given a stringified sentence, parse it and return the data in relevant data structures.
+    Input:
+        sentence: list of strings representing the given sentence, in the format standardized for the course, e.g.
+            "DDI-DrugBank.d695.s0	While	0	4	O	form=While	formlower=while	suf3=ile	suf4=hile"
+    Output:
+        tokens: list of tuples representing tokens: (word, offset_from, offset_to)
+        features: list of features for every token
+        tags: list of tags (e.g. B-drug, O, I-brand etc.) for every token
+    '''
+    tags = []
+    features = []
+    tokens = []
+    for token in sentence:
+        split_data = token[:-1].split('\t')
+        sentence_id = split_data[0]
+        features.append(split_data[5:])
+        tags.append(split_data[4])
+        tokens.append((split_data[1], split_data[2], split_data[3]))
+    return tokens, features, tags
+
+def read_feature_file(filepath):
+    '''
+    Task:
+        Given the path to the file containing tokenized sentences (in the format standardized for the course)
+    Input:
+        filepath: Path to the data
+    Output:
+        metadata: list of tuples: (sentence_id, list_of_tokens). Each token represents a sentence,
+            where in the list_of_tokens each token is represented by a tuple (word, offset_from, offset_to)
+        features: list of lists of features per sentence
+        tags: list of lists of tags (e.g. B-drug, O, I-brand etc.) per sentence
+    '''
+    features = []
+    tags = []
+    metadata=[]
+    
+    f = open(filepath, 'r')
+    lines = f.readlines()
+    sentences = groupby(lines, lambda l: l.split('\t')[0])
+
+    for sid, sentence in sentences:
+        s_tokens, s_features, s_tags = parse_sentence_string(sentence)
+        features.append(s_features)
+        tags.append(s_tags)
+        metadata.append((sid, s_tokens))
+           
+    return metadata, features, tags
